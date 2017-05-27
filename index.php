bc5af272b542e7e0e5ae94d80e53059b29bf9923
panjisula <panji.sula@gmail.com>
2017-05-27 11:36:14 +0700

    test nambah fungsi keliling persegi

--- index.php
+++ index.php
@@ -2,5 +2,12 @@
 
 echo luas_segitiga(2,3);
 echo luas_persegi();
-echo keliling_persegi();
+echo keliling_persegi(4);
 echo diameter_lingkaran();
+
+
+
+function keliling_persegi($sisi){
+    $kel = 2($sisi+$sisi);
+    return $kel;
+}
